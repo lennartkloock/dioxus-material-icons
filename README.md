62b5b0804813e525cea49305ff1d6bc5686d6dde
Lennart Kloock <lennart.kloock@protonmail.com>
2023-01-21 02:26:41 +0100

    Add README.md and docs

---
+++ README.md
@@ -0,0 +1,52 @@
+# Dioxus Material Icons
+
+This project provides a simple but configurable component to render Google's Material Icons in Dioxus.
+
+## How to get started
+
+`cargo add dioxus-material-icons`
+
+This project introduces two components:
+
+1. `MaterialIconStylesheet`
+2. `MaterialIcon`
+
+To be able to use the `MaterialIcon` component anywhere in your code, you first have to include
+a `MaterialIconStylesheet` component. When you want to use the default settings, just add it to your `App` component
+like this:
+
+```
+MaterialIconStylesheet { }
+```
+
+Have a look at the docs for more options like self-hosting the icon font file.
+
+After that you can use the `MaterialIcon` component like you would expect it:
+
+```
+MaterialIcon { name: "face" }
+```
+
+You can additionally specify the color and size.
+
+```
+MaterialIcon {
+    name: "settings",
+    size: 24,
+    color: MaterialIconColor::Light,
+}
+```
+
+## Examples
+
+- [Button Example](https://github.com/lennartkloock/dioxus-material-icons/blob/main/examples/button.rs)
+
+## Useful links
+
+- [Overview of all icons](https://fonts.google.com/icons?selected=Material+Icons) (including names)
+
+### Alternatives
+
+- [dioxus-free-icons](https://crates.io/crates/dioxus-free-icons) (Support for other icon packs)
+
+&copy; Lennart Kloock
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,58 @@
 #![allow(non_snake_case)]
 
+//! # Dioxus Material Icons
+//!
+//! This project provides a simple but configurable component to render Google's Material Icons in Dioxus.
+//!
+//! ## How to get started
+//!
+//! `cargo add dioxus-material-icons`
+//!
+//! This project introduces two components:
+//!
+//! 1. [`MaterialIconStylesheet`](MaterialIconStylesheet)
+//! 2. [`MaterialIcon`](MaterialIcon)
+//!
+//! To be able to use the [`MaterialIcon`](MaterialIcon) component anywhere in your code, you first have to include
+//! a [`MaterialIconStylesheet`](MaterialIconStylesheet) component. When you want to use the default settings, just add it to your `App` component
+//! like this:
+//!
+//! ```no_rust
+//! MaterialIconStylesheet { }
+//! ```
+//!
+//! Have a look at the docs for more options like self-hosting the icon font file.
+//!
+//! After that you can use the `MaterialIcon` component like you would expect it:
+//!
+//! ```no_rust
+//! MaterialIcon { name: "face" }
+//! ```
+//!
+//! You can additionally specify the color and size.
+//!
+//! ```no_rust
+//! MaterialIcon {
+//!     name: "settings",
+//!     size: 24,
+//!     color: MaterialIconColor::Light,
+//! }
+//! ```
+//!
+//! ## Examples
+//!
+//! - [Button Example](https://github.com/lennartkloock/dioxus-material-icons/blob/main/examples/button.rs)
+//!
+//! ## Useful links
+//!
+//! - [Overview of all icons](https://fonts.google.com/icons?selected=Material+Icons) (including names)
+//!
+//! ### Alternatives
+//!
+//! - [dioxus-free-icons](https://crates.io/crates/dioxus-free-icons) (Support for other icon packs)
+//!
+//! &copy; Lennart Kloock
+
 use dioxus::prelude::*;
 
 #[derive(Props, PartialEq)]
